27d99ad77faadfa94f75ec607413bdc8be7b611e
Micahgard <micahgardner7@gmail.com>
2020-12-14 18:50:35 +1300

    seperated functions

--- webScraper.py
+++ webScraper.py
@@ -1,29 +1,40 @@
-from bs4 import BeautifulSoup
-
 import requests
+from bs4 import BeautifulSoup
+# Circular import below fails
+#import runProgram
 
-url = input(f"Enter a website to scrape: \n")
+def runDesc():
+    
+    #could potentially refactor variables to description over title as function pulls description
+    url = input(f"Enter a website to scrape: \n")
 
-scrapedSite = requests.get("http://" + url)
+    scrapedSite = requests.get("http://" + url)
 
-data = scrapedSite.text
+    data = scrapedSite.text
 
-soup = BeautifulSoup(data)
+    soup = BeautifulSoup(data)
 
-webTitle = soup.title
-webMeta = soup.find_all('meta')
+    webTitle = soup.title
+    webMeta = soup.find_all('meta')
 
-print("Website Title: ", webTitle)
+    print("Website Title: ", webTitle)
 
-for meta in webMeta:
-    if meta.has_attr('property'):
-        if meta['property'] == 'og:description':
-            print(meta)
+    for meta in webMeta:
+        if meta.has_attr('property'):
+            if meta['property'] == 'og:description':
+                print(meta)
+    # Want to give choice to user to restart program
+    #    runProgram.runChoice()
 
+def runLinks():
+    url = input(f"Enter a website to scrape: \n")
+    scrapedSite = requests.get("http://" + url)
+    data = scrapedSite.text
 
-# email = soup.find_all(id=True)
-# print(email)
+    soup = BeautifulSoup(data)
+    
+    for a in soup.find_all('a', href=True):
+        print ("Found the URL:", a['href'])
 
-
-for a in soup.find_all('a', href=True):
-    print ("Found the URL:", a['href'])
+    # Want to give choice to user to restart program
+    # runProgram.runChoice()
